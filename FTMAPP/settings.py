b657e632a675d5ffc2561b6ac203c3f3c214844e
Yakub Ajibade <ajibadeyakubadebare@gmail.com>
2023-05-24 08:46:35 +0100

    allow host

--- FTMAPP/settings.py
+++ FTMAPP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%(2#i#(a(f+rpt$gqkn=cg1vz-o)z6977xg&lludt&is&o15eq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
